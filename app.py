4742ad00bfed2a445fcc6c176bfbbeb6ede21077
Rakshan Hegde <69235121+var787@users.noreply.github.com>
2021-11-13 16:03:37 -0600

    completed

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 # String Concatenation
+
+adjective_1 = input('Adjective: ')
+adjective_2 = input('Another Adjective: ')
+verb = input('Verb Please: ')
+
+print(f"""It's a {adjective_1} day today and I am so {adjective_2}. 
+I just want to {verb} around""")
